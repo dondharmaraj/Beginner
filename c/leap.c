517514766d2ad9327d02bb9ac865b4399c11c62d
dondharmaraj <40389000+dondharmaraj@users.noreply.github.com>
2018-07-31 10:02:49 +0000

    Create leap.c

---
+++ c/leap.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int year;
+
+    printf("Enter a year: ");
+    scanf("%d",&year);
+
+    if(year%4 == 0)
+    {
+        if( year%100 == 0)
+        {
+            if ( year%400 == 0)
+                printf("Yes");
+            else
+                printf("No");
+        }
+        else
+            printf("Yes");
+    }
+    else
+        printf("No");
+    
+    return 0;
+}
