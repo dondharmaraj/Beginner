d6abbab86876f4cc887d98a932469d91babd9044
dondharmaraj <40389000+dondharmaraj@users.noreply.github.com>
2018-08-07 11:05:31 +0000

    Create Armstrongbetween.c

---
+++ c/Armstrongbetween.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int l, high, i, temp1, temp2, remainder, n = 0, result = 0;
+
+    scanf("%d %d", &l, &high);
+
+    for(i = l + 1; i < high; ++i)
+    {
+        temp2 = i;
+        temp1 = i;
+
+        while (temp1 != 0)
+        {
+            temp1 /= 10;
+            ++n;
+        }
+
+        while (temp2 != 0)
+        {
+            remainder = temp2 % 10;
+            result += pow(remainder, n);
+            temp2 /= 10;
+        }
+
+        if (result == i) {
+            printf("%d ", i);
+        }
+
+        n = 0;
+        result = 0;
+
+    }
+    return 0;
+}
