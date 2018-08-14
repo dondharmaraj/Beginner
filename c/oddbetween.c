fa69ef13baacd559eae07536af6c7f0bb65fc316
dondharmaraj <40389000+dondharmaraj@users.noreply.github.com>
2018-08-14 10:52:38 +0000

    Create oddbetween.c

---
+++ c/oddbetween.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int N,Q,i,rem;
+    scanf("%d",&N);
+    scanf("%d",&Q);
+    if(N<=100000&&Q<=100000)
+    {
+    for (i=N+1;i<Q;i++)
+        {
+            rem=i%2;
+            if(rem!=0)
+                printf("%d",i);
+        }
+    }
+}
