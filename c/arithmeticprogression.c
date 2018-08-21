f9ae2ea77b7cc591de24491a83f029c0a9fc5f7d
dondharmaraj <40389000+dondharmaraj@users.noreply.github.com>
2018-08-21 10:27:55 +0000

    Update arithmeticprogression.c

--- c/arithmeticprogression.c
+++ c/arithmeticprogression.c
@@ -1,16 +1,24 @@
 #include<stdio.h>
 void main()
 {
-        int Z=0,N,A,D,i;
+        int Z=0,N,A,D,i,sum,arr[20];
         scanf("%d",&N);
         scanf("%d",&A);
         scanf("%d",&D);
         if((N>=1&&N<=100000)&&(A>=1&&A<=100000)&&(D>=1&&D<=100000))
         {
-            for(i=0;i<N;i++)
+            arr[0]=A;
+            Z=A+D;
+            for(i=1;i<N;i++)
             {
-                Z=Z+A+D;
+                arr[i]=Z;
+                Z=Z+D;
             }
-            printf("%d",Z);
+            sum=arr[0]+arr[1];
+            for(i=2;i<N;i++)
+            {
+                sum=sum+arr[i];
+            }
+            printf("%d",sum);
         }
 }
